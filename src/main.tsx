e1ee98a9a6f539b55d98339ce96fbb316469f2db
Jordi <jordi.casas00@gmail.com>
2024-09-05 09:03:50 +0200

    feat: Update drawing functionality to support clearing the canvas

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import './main.css';
 import HausesDraw from '../lib/draw/src/index';
 import { ToggleGroup,ToggleGroupItem } from "./components/ui/toggle-group";
 import { Lock, Move, MousePointer, Square, Circle, Minus, Diamond, MoveRight, Text, Eraser } from "lucide-react";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 export function Main() {
   const ref = useRef(null);
+  const [selectedTool, setSelectedTool] = useState<string>('move');
+  const [hausesDraw, setHausesDraw] = useState<HausesDraw | null>(null);
+  const size = useWindowSize();
   const [tools, setTools] = useState<string[]>([]);
   const TOOLS : any = {
-    'move': <Move />,
+    'move': <Move size={16}/>,
     'select': <MousePointer />,
     'rectangle': <Square />,
     'ellipse': <Circle />,
@@ -22,25 +26,37 @@ export function Main() {
 
   useEffect(() => {
     if (ref.current) {
-      const draw = new HausesDraw(ref.current);
+      const draw = new HausesDraw(ref.current,size.width, size.height);
       setTools(Object.values(draw.getTools()));
+      setHausesDraw(draw);
     }
   }
-  , [ref]);
+  , [ref, size]);
 
-  console.log(tools)
-  
+  const handleSelect = (tool: string) => {
+    if (hausesDraw) {
+      hausesDraw.setTool(tool);
+      setSelectedTool(tool);
+    }
+  }
+
+ const handleChange = (value: string) => {
+    console.log(value);
+    handleSelect(value);
+  } 
   return (
     <div className="w-screen h-screen relative">
-     <canvas ref={ref} width="800" height="600"/>
+     <canvas ref={ref} width={size.width || 800} height={size.height || 800}/>
      <div className='absolute top-5 left-1/2 transform -translate-x-1/2 bg-muted rounded-md z-40 w-fit'>
-     <ToggleGroup  type='single'>
+     <ToggleGroup  type='single' onValueChange={handleChange}>
         {tools.map((tool) => (
           <ToggleGroupItem value={tool} key={tool}>
             {TOOLS[tool]}
           </ToggleGroupItem>
         ))}
      </ToggleGroup>
+      </div>
+      <div>
       </div>
     </div>
   );
